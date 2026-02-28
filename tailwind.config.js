110edd855233c12672a1a912652aebc5aaf1e296
na-x-ril <nazrilpro10@gmail.com>
2026-02-28 21:50:01 +0700

    refactor(build): - fix wrong tailwindcss config content

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./App.{js,ts,tsx}', './components/**/*.{js,ts,tsx}'],
-
+  content: ['./app/**/*.{js,jsx,ts,tsx}', './src/**/*.{js,jsx,ts,tsx}'],
   presets: [require('nativewind/preset')],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
 };
